3a0a76998ab4df4470c525a0b8078f974fef9451
Endrit Hoda <endrit.hoda@student.uni-pr.edu>
2022-01-12 23:10:52 +0100

    Ndryshime

--- asset/js/ballina.js
+++ asset/js/ballina.js
@@ -6,15 +6,6 @@ function Bright_night() {
                 document.body.style.backgroundColor="#294525";
         } 
     }
-clickCounter();
 
-function clickCounter() {
-  if (localStorage.clickcount) {
-    localStorage.clickcount = Number(localStorage.clickcount)+1;
-  } else {
-    localStorage.clickcount = 1;
-  }
-  document.getElementById("demo").innerHTML = localStorage.clickcount;
-}
 
     
